c78ab66b394491304f9775a014cf711a055d4246
Akshara Gadwe <2016.akshara.gadwe@ves.ac.in>
2018-12-21 06:34:22 -0800

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+        <h1> This is About
+        </h1>
+        <p> fgfgfgfg</p>
+        @endsection
+
+
+
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+        <h1> This is Services 
+        </h1>
+        <p> fgfgfgfg</p>
+@endsection
+
+
